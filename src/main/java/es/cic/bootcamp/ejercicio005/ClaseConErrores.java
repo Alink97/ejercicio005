c3a182b3a8e6407ebe76a4517901d76a7a1eabab
Alink97 <alejandro_fg97@icloud.com>
2021-07-15 11:05:56 +0200

    Ejemplo de uso y antiejemplo de excepciones

---
+++ src/main/java/es/cic/bootcamp/ejercicio005/ClaseConErrores.java
@@ -0,0 +1,18 @@
+package es.cic.bootcamp.ejercicio005;
+
+import java.io.IOException;
+
+public class ClaseConErrores {
+	
+	public void metodo1() {
+		throw new NoImplementadoException("Esto es un error");
+	}
+	
+	public int metodo2(boolean procesa) throws MotorException, IOException {
+		if(procesa) {
+			throw new MotorException("Esto lanza un error de motor", 5);
+		} else {
+			throw new IOException("Esto lanza un error de tipo IO");
+		}
+	}
+}
---
+++ src/main/java/es/cic/bootcamp/ejercicio005/NoImplementadoException.java
@@ -0,0 +1,20 @@
+package es.cic.bootcamp.ejercicio005;
+
+public class NoImplementadoException extends RuntimeException {
+
+	public NoImplementadoException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoImplementadoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoImplementadoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}

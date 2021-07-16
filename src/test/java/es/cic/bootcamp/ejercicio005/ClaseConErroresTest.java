41c249d862b214f18b225d810d7b51062819f66c
Alink97 <alejandro_fg97@icloud.com>
2021-07-16 10:19:31 +0200

    Ejemplos finally, try con recursos, creacion de ficheros temporales, acceso a recurso del classpath

--- src/test/java/es/cic/bootcamp/ejercicio005/ClaseConErroresTest.java
+++ src/test/java/es/cic/bootcamp/ejercicio005/ClaseConErroresTest.java
@@ -2,7 +2,11 @@ package es.cic.bootcamp.ejercicio005;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,22 +14,38 @@ import org.junit.jupiter.api.Test;
 class ClaseConErroresTest {
 
 	private ClaseConErrores cut;
+	private String nombreFichero;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		this.cut = new ClaseConErrores();
+		
+		File ftemporal = File.createTempFile("hola", ".txt");
+		nombreFichero = ftemporal.getAbsolutePath();
+		try (PrintWriter pw = new PrintWriter(ftemporal)){
+			pw.println("hola mundo");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 	@Test
 	void testMetodo1() {
-		cut.metodo1();
+		assertThrows(NoImplementadoException.class, 
+				() -> cut.metodo1());
 	}
 
 	@Test
 	void testMetodo2() throws IOException {
 		
-		try {
-			cut.metodo2(false);
+		try (InputStream fis = ClaseConErroresTest.class.getResourceAsStream("/hola.txt")) {
+			byte[] datos = new byte[64 * 1024];
+			int cuantos;
+			while ((cuantos = fis.read(datos)) != -1) {
+				
+			}
+			
+			cut.metodo2(true);
 		} catch (MotorException e) {			
 			e.printStackTrace();
 			
@@ -35,11 +55,13 @@ class ClaseConErroresTest {
 	
 	@Test
 	void testMetodo2EncapsulandoElError() throws CocheException {
-		try {
-			cut.metodo2(false);
-		} catch (MotorException | IOException e) {
-			throw new CocheException("El coche no arranca", e);
-		}
+		assertThrows(CocheException.class, () -> {
+			try {
+				cut.metodo2(false);
+			} catch (MotorException | IOException e) {
+				throw new CocheException("El coche no arranca", e);
+			}
+		});
 	}
 
 }
